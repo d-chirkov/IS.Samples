955720a32dda5e3b9effd1ff57a3b1fd9206f409
d-chirkov <d.chirkov.94@outlook.com>
2019-06-26 20:55:11 +0300

    Extension for configuration

---
+++ IdSrv.Connector/AuthServerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace IdSrv.Connector
+{
+    internal class AuthServerConfiguration
+    {
+        public string IdSrvAddress { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string OwnAddress { get; set; }
+
+        public bool IsComplete {
+            get
+            {
+                return 
+                    this.IdSrvAddress != null && 
+                    this.ClientId != null && 
+                    this.ClientSecret != null && 
+                    this.OwnAddress != null;
+            }
+        }
+    }
+}
