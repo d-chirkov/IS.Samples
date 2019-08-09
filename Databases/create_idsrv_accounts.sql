61b3b6d79cf17b006edbcd335721e231957f776b
d-chirkov <d.chirkov.94@outlook.com>
2019-08-10 00:00:04 +0300

    Скрипты для создания SqlCe баз данных.

---
+++ Databases/create_idsrv_accounts.sql
@@ -0,0 +1,13 @@
+﻿-- Script Date: 09.08.2019 23:55  - ErikEJ.SqlCeScripting version 3.5.2.81
+CREATE TABLE [Clients] (
+  [Id] uniqueidentifier DEFAULT (((NEWID()))) NOT NULL
+, [Name] nvarchar(1024) NOT NULL
+, [Uri] nvarchar(4000) NULL
+, [Secret] nvarchar(1024) NOT NULL
+, [IsBlocked] bit DEFAULT (((0))) NOT NULL
+);
+GO
+ALTER TABLE [Clients] ADD CONSTRAINT [PK_Clients] PRIMARY KEY ([Id]);
+GO
+ALTER TABLE [Clients] ADD CONSTRAINT [UQ__Clients__000000000000006B] UNIQUE ([Name]);
+GO
---
+++ Site1.Mvc5/create_accounts.sql
@@ -0,0 +1,11 @@
+﻿-- Script Date: 09.08.2019 23:58  - ErikEJ.SqlCeScripting version 3.5.2.81
+CREATE TABLE [UserProfiles] (
+  [UserProfileId] int IDENTITY (1,1) NOT NULL
+, [IdSrvId] nvarchar(100) NOT NULL
+, [Login] nvarchar(256) NOT NULL
+);
+GO
+ALTER TABLE [UserProfiles] ADD CONSTRAINT [PK_UserProfiles] PRIMARY KEY ([UserProfileId]);
+GO
+ALTER TABLE [UserProfiles] ADD CONSTRAINT [UQ__UserProfiles__000000000000006B] UNIQUE ([IdSrvId]);
+GO
