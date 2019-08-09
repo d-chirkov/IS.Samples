854cfbb3aea9ffe9b32e27773f9c5ba80908588d
d-chirkov <d.chirkov.94@outlook.com>
2019-08-10 00:01:15 +0300

    В create_idsrv_accounts.sql добавил создание таблицы пользователей.

--- Databases/create_idsrv_accounts.sql
+++ Databases/create_idsrv_accounts.sql
@@ -11,3 +11,17 @@ ALTER TABLE [Clients] ADD CONSTRAINT [PK_Clients] PRIMARY KEY ([Id]);
 GO
 ALTER TABLE [Clients] ADD CONSTRAINT [UQ__Clients__000000000000006B] UNIQUE ([Name]);
 GO
+
+-- Script Date: 09.08.2019 23:55  - ErikEJ.SqlCeScripting version 3.5.2.81
+CREATE TABLE [Users] (
+  [Id] uniqueidentifier DEFAULT ((((NEWID())))) NOT NULL
+, [UserName] nvarchar(256) NOT NULL
+, [PasswordHash] nvarchar(1024) NULL
+, [PasswordSalt] nvarchar(1024) NULL
+, [IsBlocked] bit DEFAULT (0) NOT NULL
+);
+GO
+ALTER TABLE [Users] ADD CONSTRAINT [PK__Users__000000000000005A] PRIMARY KEY ([Id]);
+GO
+ALTER TABLE [Users] ADD CONSTRAINT [UQ__Users__000000000000006B] UNIQUE ([UserName]);
+GO
